a34fd4f743ff3b485eade0fb5f1c4e90b4458751
Samkit Sheth <samkitsheth95@gmail.com>
2020-08-26 01:38:39 -0700

    Problem 844 Backspace String Compare

---
+++ com/leetcode/backspaceCompare844a.java
@@ -0,0 +1,30 @@
+/* Problem - https://leetcode.com/problems/backspace-string-compare/ */
+class backspaceCompare844a{
+
+    private static String backspaceCompareA(String S){
+        int skip = 0;
+        StringBuilder s = new StringBuilder();
+        for (int i = S.length() - 1; i >= 0; i--) {
+            if ( S.charAt(i) == '#' ) {
+                skip++;
+            } else {
+                if ( skip > 0 ) {
+                    skip--;
+                } else {
+                    s.append( S.charAt(i) );
+                }
+            }
+        }
+        return S.toString();
+    }
+
+    private static boolean backspaceCompare(String S, String T) {
+        return backspaceCompareA(S).equals(backspaceCompareA(T));
+    }
+
+    public static void main(String[] args){
+        System.out.println(backspaceCompare("bxj##tw",
+        "bxj###tw"));
+    }
+
+}
