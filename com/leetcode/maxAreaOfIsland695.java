1a7e0a22fd4046b864f3f26d5bd944b38acc7ac3
Samkit Sheth <samkitsheth95@gmail.com>
2020-08-27 16:29:46 -0700

    Leetcode 695. Max Area of Island

---
+++ com/leetcode/maxAreaOfIsland695.java
@@ -0,0 +1,37 @@
+class maxAreaofIsland695 {
+
+    private static int findAllOne(int[][] grid, boolean[][] seen, int i, int j,int sum) {
+        if (i < 0 || j<0 || i >= grid.length || j>= grid[0].length) return sum;
+        if (seen[i][j]) return sum;
+        seen[i][j] = true;
+        if (grid[i][j] == 0){ return sum; }
+        sum = sum + 1 +
+        findAllOne(grid, seen, i - 1, j,sum) +
+        findAllOne(grid, seen, i + 1, j,sum) +
+        findAllOne(grid, seen, i, j + 1,sum) +
+        findAllOne(grid, seen, i, j - 1,sum);  
+        return sum;     
+    }
+
+    private static int maxAreaOfIsland(int[][] grid) {
+        int sum = 0;
+        boolean[][] seen = new boolean[grid.length][grid[0].length];
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (!seen[i][j] && grid[i][j] == 1) {
+                    sum = Math.max(sum, findAllOne(grid, seen, i, j, 0));
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] x = new int[][] { { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                { 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },
+                { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 } };
+        System.out.println(maxAreaOfIsland(x));
+    }
+}
