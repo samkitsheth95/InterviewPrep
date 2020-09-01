4b7ed2a1d8b3d5404995b81d7e974a279fe1eb00
Samkit Sheth <samkitsheth95@gmail.com>
2020-08-31 22:56:13 -0700

    leetcode 937. Reorder Data in Log Files

---
+++ com/leetcode/937.ReorderDatainLogFiles.py
@@ -0,0 +1,16 @@
+def reorderLogFiles(logs):
+    let = []
+    dig = []
+    for i in logs:
+        if i[-1].isdigit():
+            dig.append(i)
+        else:
+            temp = i.split(" ")
+            let.append([temp[0],temp[1:]])
+    let.sort(key = lambda x : (x[1],x[0]))
+    result = []
+    for i in let:
+        result.append(i[0] + " " + " ".join(i[1]))        
+    return result + dig
+
+print(reorderLogFiles(["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]))
