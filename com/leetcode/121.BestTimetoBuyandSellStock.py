d6e0ee3fdcec1283488a332cce9c619538eb8db0
Samkit Sheth <samkitsheth95@gmail.com>
2020-09-02 22:18:35 -0700

    leetcode 121. Best Time to Buy and Sell Stock

---
+++ com/leetcode/121.BestTimetoBuyandSellStock.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        maxProfit = 0
+        profit = 0
+        for i in range(1, len(prices)):
+            profit = max(profit+prices[i]-prices[i-1], 0)
+            maxProfit = max(maxProfit, profit)
+        return maxProfit
+
+
+sol = Solution()
+print(sol.maxProfit([7,1,5,3,6,4]))
+    
