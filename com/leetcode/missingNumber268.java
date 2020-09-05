624f1a955d8ccddeb9e12f64486cb7fb20d0e29f
Samkit Sheth <samkitsheth95@gmail.com>
2020-09-04 21:43:32 -0700

    leetcode 268.Missing Number

---
+++ com/leetcode/missingNumber268.java
@@ -0,0 +1,15 @@
+class missingNumber268 {
+
+    public static int missingNumber(int[] nums) {
+        int realsum = nums.length * (nums.length + 1) / 2;
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        return realsum - sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(missingNumber(new int[]{9,6,4,2,3,5,7,0,1}));
+    }
+}
