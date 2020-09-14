014166ebb197941f7347d1a9a675c1bd2dc51b94
Samkit Sheth <samkitsheth95@gmail.com>
2020-09-13 23:11:11 -0700

    leetcode 101. Symmetric Tree

---
+++ com/Leetcode/101.SymmetricTree.py
@@ -0,0 +1,14 @@
+class Solution:
+    
+    def sym(self, left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        else:
+            return left.val == right.val and self.sym(left.left,right.right) and self.sym(left.right,right.left)
+        
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        return self.sym(root.left,root.right)
