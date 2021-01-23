474b4905b2d49d2ebb0f7c12e9b9ea3a28bcffb9
Samkit Sheth <samkitsheth95@gmail.com>
2021-01-22 20:25:10 -0800

    Leetcode 21 Merge Two Sorted Lists

---
+++ com/Leetcode/21.MergeTwoSortedLists.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        fakeHead = ListNode(0, None)
+        curr = fakeHead
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l2 if l2 else l1
+        return fakeHead.next
