0b0bb63176e095351c59428b7fdf5f576983b49e
Samkit Sheth <samkitsheth95@gmail.com>
2021-01-24 23:28:54 -0800

    Leetcode 215 Kth Largest Element in an Array

--- com/Leetcode/215.KthLargestElementinanArray.py
+++ com/Leetcode/215.KthLargestElementinanArray.py
@@ -1,9 +1,39 @@
 from typing import List
 from collections import Counter
+from random import randint
 import heapq
 
 
 class Solution:
+
+    def findPivot(self, nums, low, high):
+        pivotLoc = randint(low, high)
+        pivot = nums[pivotLoc]
+        nums[low], nums[pivotLoc] = nums[pivotLoc], nums[low]
+        pivotLoc = low
+        low += 1
+        while low <= high:
+            while low <= high and nums[low] > pivot:
+                low += 1
+            while low <= high and nums[high] <= pivot:
+                high -= 1
+            if low <= high:
+                nums[low], nums[high] = nums[high], nums[low]
+        nums[high], nums[pivotLoc] = nums[pivotLoc], nums[high]
+        return high
+
+    def quickselect(self, nums, low, high, k):
+        if low <= high:
+            pivot = self.findPivot(nums, low, high)
+            if pivot == k:
+                return nums[pivot]
+            elif pivot > k:
+                return self.quickselect(nums, low, pivot - 1, k)
+            else:
+                return self.quickselect(nums, pivot + 1, high, k)
+
+    def findKthLargestQuickSelect(self, nums: List[int], k: int) -> int:
+        return self.quickselect(nums, 0, len(nums) - 1, k - 1)
 
     def findKthLargest(self, nums: List[int], k: int) -> int:
         freq = Counter(nums).items()
@@ -18,4 +48,4 @@ class Solution:
 
 
 sol = Solution()
-print(sol.findKthLargestHeap(nums=[3, 2, 1, 5, 6, 4], k=2))
+print(sol.findKthLargestQuickSelect(nums=[3, 2, 1, 5, 6, 4], k=2))
