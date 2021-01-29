13c4648b45af925cfe79d8fa473122af692865ac
Samkit Sheth <samkitsheth95@gmail.com>
2021-01-28 22:54:41 -0800

    Leetcode 1344 Angle Between Hands of a Clock

---
+++ com/Leetcode/1344.AngleBetweenHandsofaClock.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def angleClock(self, hour: int, minutes: int) -> float:
+        am = minutes * 6
+        ah = ((hour * 30) + (minutes/60) * 30) % 360
+        if am > ah:
+            return min(am - ah, (360 - am) + ah)
+        else:
+            return min(ah - am, (360 - ah) + am)
+
+    def angleClockLeetcodeSol(self, hour: int, minutes: int) -> float:
+        one_min_angle = 6
+        one_hour_angle = 30
+
+        minutes_angle = one_min_angle * minutes
+        hour_angle = (hour % 12 + minutes / 60) * one_hour_angle
+
+        diff = abs(hour_angle - minutes_angle)
+        return min(diff, 360 - diff)
+
+
+sol = Solution()
+print(sol.angleClock(hour=12, minutes=30))
