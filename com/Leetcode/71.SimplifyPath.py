68353a101ac483d979e78153d74c110bedb84a86
Samkit Sheth <samkitsheth95@gmail.com>
2021-02-01 19:24:55 -0800

    Leetcode 71 Simplify Path

---
+++ com/Leetcode/71.SimplifyPath.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        for i in path.split("/"):
+            if i == "..":
+                if stack:
+                    stack.pop()
+            elif i and i != ".":
+                stack.append(i)
+        return "/" + "/".join(stack)
+
+
+sol = Solution()
+print(sol.simplifyPath("/a/./b/../../c/"))
