491288500f28aa946c8b3a8016e9889c4bed56f4
Samkit Sheth <samkitsheth95@gmail.com>
2021-04-17 21:47:24 -0700

    Leetcode 455 Assign Cookies

---
+++ com/Leetcode/455.AssignCookies.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i, j = 0, 0
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                i += 1
+            j += 1
+        return i
+
+
+sol = Solution()
+print(sol.findContentChildren([10, 9, 8, 7], [5, 6, 7, 8]))
